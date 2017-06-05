3df9e77d87eac1c4ebf58279d0dda7ad8514d665
PedronBigodon111 <anonymousxixid@hotmail.com>
2017-06-05 09:08:16 -0300

    Create code.c

---
+++ code.c
@@ -0,0 +1,128 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+int main(){
+    int a=40 , b=40, i, j, x=-126, y=-120, z=-58, a1=-121, a2=-96, a3=-94;
+    int matriz[a][b];
+    char resp;
+
+
+    system("color 2");
+    printf("\n\nMor eu fiz isso pra tu saber que n%co te esqueci e que te amo um tant%coo :)\n", z, z);
+    printf("Mas antes, vamos a um testezinho de l%cgica!\n", a3);
+    system("pause");
+    system("cls");
+    printf("\"Elefantes sempre usam cal%cas cor-de-rosa.\"\n\"Toda criatura que come mel sabe tocar gaita de fole.\"\n", a1);
+    printf("\"Tudo o que %c f%ccil de engolir come mel.\"\n\n\"Nenhuma criatura que usa cal%cas cor-de-rosa sabe tocar gaita de fole.\"\n\n\n", x, a2, a1);
+    printf("Portanto: \nOs elefantes s%co f%cceis de engolir.\n Esta afirma%c%co est%c correta ou n%co?\n", z, a2, a1, z, a2, z, z);
+    scanf("%c", &resp);
+
+    if(resp=='s'){
+        printf("\n Saindo... :(\n");
+        system("pause");
+        exit(0);
+    }
+
+    else
+        system("cls");
+
+    for(i=0; i<a; i++)
+        for(j=0; j<b; j++){
+            if(i==j)
+                matriz[i][j]=0;
+            else
+                matriz[i][j]=0;
+        }
+    printf("\n");
+    for(i=0; i<a; i++)
+        for(j=0; j<b; j++){
+            printf("\t%d",matriz[j][i]);
+            if(j==b-1)
+                printf("\n");
+                matriz[19][30]=1;matriz[31][13]=1;
+                matriz[17][29]=1;matriz[18][29]=1;matriz[19][29]=1;matriz[20][29]=1;matriz[21][29]=1;
+                matriz[15][28]=1;matriz[16][28]=1;matriz[17][28]=1;matriz[18][28]=1;matriz[19][28]=1;matriz[20][28]=1;matriz[21][28]=1;matriz[22][28]=1;matriz[23][28]=1;
+                matriz[13][27]=1;matriz[14][27]=1;matriz[15][27]=1;matriz[16][27]=1;matriz[17][27]=1;matriz[18][27]=1;matriz[19][27]=1;matriz[20][27]=1;matriz[21][27]=1;matriz[22][27]=1;matriz[25][27]=1;
+                matriz[11][26]=1;matriz[12][26]=1;matriz[13][26]=1;matriz[14][26]=1;matriz[15][26]=1;matriz[16][26]=1;matriz[17][26]=1;matriz[18][26]=1;matriz[19][26]=1;matriz[20][26]=1;matriz[21][26]=1;matriz[27][26]=1;
+                matriz[9][25]=1;matriz[10][25]=1;matriz[11][25]=1;matriz[12][25]=1;matriz[13][25]=1;matriz[14][25]=1;matriz[15][25]=1;matriz[16][25]=1;matriz[17][25]=1;matriz[29][25]=1;
+                matriz[7][24]=1;matriz[8][24]=1;matriz[9][24]=1;matriz[10][24]=1;matriz[11][24]=1;matriz[12][24]=1;matriz[13][24]=1;matriz[14][24]=1;matriz[15][24]=1;matriz[31][24]=1;
+                matriz[5][23]=1;matriz[6][23]=1;matriz[7][23]=1;matriz[8][23]=1;matriz[9][23]=1;matriz[10][23]=1;matriz[11][23]=1;matriz[12][23]=1;matriz[13][23]=1;matriz[33][23]=1;
+                matriz[3][22]=1;matriz[4][22]=1;matriz[5][22]=1;matriz[6][22]=1;matriz[7][22]=1;matriz[8][22]=1;matriz[9][22]=1;matriz[10][22]=1;matriz[11][22]=1;matriz[35][22]=1;
+                matriz[1][21]=1;matriz[2][21]=1;matriz[3][21]=1;matriz[4][21]=1;matriz[5][21]=1;matriz[6][21]=1;matriz[7][21]=1;matriz[8][21]=1;matriz[9][21]=1;matriz[37][21]=1;
+                matriz[0][20]=1;matriz[1][20]=1;matriz[2][20]=1;matriz[3][20]=1;matriz[4][20]=1;matriz[5][20]=1;matriz[6][20]=1;matriz[7][20]=1;matriz[8][20]=1;matriz[39][20]=1;
+                matriz[0][19]=1;matriz[1][19]=1;matriz[2][19]=1;matriz[3][19]=1;matriz[4][19]=1;matriz[5][19]=1;matriz[6][19]=1;matriz[7][19]=1;matriz[8][19]=1;matriz[39][19]=1;
+                matriz[0][18]=1;matriz[1][18]=1;matriz[2][18]=1;matriz[3][18]=1;matriz[4][18]=1;matriz[5][18]=1;matriz[6][18]=1;matriz[7][18]=1;matriz[8][18]=1;matriz[39][18]=1;
+                matriz[0][17]=1;matriz[1][17]=1;matriz[2][17]=1;matriz[3][17]=1;matriz[4][17]=1;matriz[5][17]=1;matriz[6][17]=1;matriz[7][17]=1;matriz[8][17]=1;matriz[39][17]=1;
+                matriz[1][16]=1;matriz[2][16]=1;matriz[3][16]=1;matriz[4][16]=1;matriz[5][16]=1;matriz[6][16]=1;matriz[7][16]=1;matriz[8][16]=1;matriz[9][16]=1;matriz[38][16]=1;
+                matriz[2][15]=1;matriz[3][15]=1;matriz[4][15]=1;matriz[5][15]=1;matriz[6][15]=1;matriz[7][15]=1;matriz[8][15]=1;matriz[9][15]=1;matriz[10][15]=1;matriz[37][15]=1;
+                matriz[3][14]=1;matriz[4][14]=1;matriz[5][14]=1;matriz[6][14]=1;matriz[7][14]=1;matriz[8][14]=1;matriz[9][14]=1;matriz[10][14]=1;matriz[11][14]=1;matriz[36][14]=1;
+                matriz[5][13]=1;matriz[6][13]=1;matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[34][13]=1;
+                matriz[6][13]=1;matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[34][13]=1;
+                matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[33][13]=1;
+                matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[32][13]=1;
+                matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[30][13]=1;
+                matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[29][13]=1;
+                matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[19][13]=1;matriz[28][13]=1;
+                matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[19][13]=1;matriz[20][13]=1;matriz[27][13]=1;
+                matriz[14][14]=1;matriz[14][14]=1;matriz[15][14]=1;matriz[16][14]=1;matriz[17][14]=1;matriz[18][14]=1;matriz[19][14]=1;matriz[20][14]=1;matriz[21][14]=1;matriz[25][14]=1;
+                matriz[16][15]=1;matriz[15][15]=1;matriz[16][15]=1;matriz[17][15]=1;matriz[18][15]=1;matriz[19][15]=1;matriz[20][15]=1;matriz[21][15]=1;matriz[22][15]=1;matriz[23][15]=1;
+                matriz[18][16]=1;matriz[17][16]=1;matriz[17][16]=1;matriz[18][16]=1;matriz[19][16]=1;matriz[20][16]=1;matriz[21][16]=1;matriz[22][16]=1;matriz[23][16]=1;matriz[21][16]=1;
+                matriz[20][17]=1;matriz[21][17]=1;matriz[18][17]=1;matriz[19][17]=1;matriz[20][17]=1;matriz[21][17]=1;matriz[22][17]=1;matriz[23][17]=1;matriz[24][17]=1;matriz[19][17]=1;
+        }
+    printf("\n\n");
+    system("cls");
+
+    for(i=0; i<a; i++)
+        for(j=0; j<b; j++){
+            if(i==j)
+                matriz[i][j]=0;
+            else
+                matriz[i][j]=0;
+        }
+    printf("\n");
+    for(i=0; i<a; i++)
+        for(j=0; j<b; j++){
+            printf("%d",matriz[j][i]);
+            if(j==b-1)
+                printf("\n");
+                matriz[19][30]=1;matriz[31][13]=1;
+                matriz[17][29]=1;matriz[18][29]=1;matriz[19][29]=1;matriz[20][29]=1;matriz[21][29]=1;
+                matriz[15][28]=1;matriz[16][28]=1;matriz[17][28]=1;matriz[18][28]=1;matriz[19][28]=1;matriz[20][28]=1;matriz[21][28]=1;matriz[22][28]=1;matriz[23][28]=1;
+                matriz[13][27]=1;matriz[14][27]=1;matriz[15][27]=1;matriz[16][27]=1;matriz[17][27]=1;matriz[18][27]=1;matriz[19][27]=1;matriz[20][27]=1;matriz[21][27]=1;matriz[22][27]=1;matriz[25][27]=1;
+                matriz[11][26]=1;matriz[12][26]=1;matriz[13][26]=1;matriz[14][26]=1;matriz[15][26]=1;matriz[16][26]=1;matriz[17][26]=1;matriz[18][26]=1;matriz[19][26]=1;matriz[20][26]=1;matriz[21][26]=1;matriz[27][26]=1;
+                matriz[9][25]=1;matriz[10][25]=1;matriz[11][25]=1;matriz[12][25]=1;matriz[13][25]=1;matriz[14][25]=1;matriz[15][25]=1;matriz[16][25]=1;matriz[17][25]=1;matriz[29][25]=1;
+                matriz[7][24]=1;matriz[8][24]=1;matriz[9][24]=1;matriz[10][24]=1;matriz[11][24]=1;matriz[12][24]=1;matriz[13][24]=1;matriz[14][24]=1;matriz[15][24]=1;matriz[31][24]=1;
+                matriz[5][23]=1;matriz[6][23]=1;matriz[7][23]=1;matriz[8][23]=1;matriz[9][23]=1;matriz[10][23]=1;matriz[11][23]=1;matriz[12][23]=1;matriz[13][23]=1;matriz[33][23]=1;
+                matriz[3][22]=1;matriz[4][22]=1;matriz[5][22]=1;matriz[6][22]=1;matriz[7][22]=1;matriz[8][22]=1;matriz[9][22]=1;matriz[10][22]=1;matriz[11][22]=1;matriz[35][22]=1;
+                matriz[1][21]=1;matriz[2][21]=1;matriz[3][21]=1;matriz[4][21]=1;matriz[5][21]=1;matriz[6][21]=1;matriz[7][21]=1;matriz[8][21]=1;matriz[9][21]=1;matriz[37][21]=1;
+                matriz[0][20]=1;matriz[1][20]=1;matriz[2][20]=1;matriz[3][20]=1;matriz[4][20]=1;matriz[5][20]=1;matriz[6][20]=1;matriz[7][20]=1;matriz[8][20]=1;matriz[39][20]=1;
+                matriz[0][19]=1;matriz[1][19]=1;matriz[2][19]=1;matriz[3][19]=1;matriz[4][19]=1;matriz[5][19]=1;matriz[6][19]=1;matriz[7][19]=1;matriz[8][19]=1;matriz[39][19]=1;
+                matriz[0][18]=1;matriz[1][18]=1;matriz[2][18]=1;matriz[3][18]=1;matriz[4][18]=1;matriz[5][18]=1;matriz[6][18]=1;matriz[7][18]=1;matriz[8][18]=1;matriz[39][18]=1;
+                matriz[0][17]=1;matriz[1][17]=1;matriz[2][17]=1;matriz[3][17]=1;matriz[4][17]=1;matriz[5][17]=1;matriz[6][17]=1;matriz[7][17]=1;matriz[8][17]=1;matriz[39][17]=1;
+                matriz[1][16]=1;matriz[2][16]=1;matriz[3][16]=1;matriz[4][16]=1;matriz[5][16]=1;matriz[6][16]=1;matriz[7][16]=1;matriz[8][16]=1;matriz[9][16]=1;matriz[38][16]=1;
+                matriz[2][15]=1;matriz[3][15]=1;matriz[4][15]=1;matriz[5][15]=1;matriz[6][15]=1;matriz[7][15]=1;matriz[8][15]=1;matriz[9][15]=1;matriz[10][15]=1;matriz[37][15]=1;
+                matriz[3][14]=1;matriz[4][14]=1;matriz[5][14]=1;matriz[6][14]=1;matriz[7][14]=1;matriz[8][14]=1;matriz[9][14]=1;matriz[10][14]=1;matriz[11][14]=1;matriz[36][14]=1;
+                matriz[5][13]=1;matriz[6][13]=1;matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[34][13]=1;
+                matriz[6][13]=1;matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[34][13]=1;
+                matriz[7][13]=1;matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[33][13]=1;
+                matriz[8][13]=1;matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[32][13]=1;
+                matriz[9][13]=1;matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[30][13]=1;
+                matriz[10][13]=1;matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[29][13]=1;
+                matriz[11][13]=1;matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[19][13]=1;matriz[28][13]=1;
+                matriz[12][13]=1;matriz[13][13]=1;matriz[14][13]=1;matriz[15][13]=1;matriz[16][13]=1;matriz[17][13]=1;matriz[18][13]=1;matriz[19][13]=1;matriz[20][13]=1;matriz[27][13]=1;
+                matriz[14][14]=1;matriz[14][14]=1;matriz[15][14]=1;matriz[16][14]=1;matriz[17][14]=1;matriz[18][14]=1;matriz[19][14]=1;matriz[20][14]=1;matriz[21][14]=1;matriz[25][14]=1;
+                matriz[16][15]=1;matriz[15][15]=1;matriz[16][15]=1;matriz[17][15]=1;matriz[18][15]=1;matriz[19][15]=1;matriz[20][15]=1;matriz[21][15]=1;matriz[22][15]=1;matriz[23][15]=1;
+                matriz[18][16]=1;matriz[17][16]=1;matriz[17][16]=1;matriz[18][16]=1;matriz[19][16]=1;matriz[20][16]=1;matriz[21][16]=1;matriz[22][16]=1;matriz[23][16]=1;matriz[21][16]=1;
+                matriz[20][17]=1;matriz[21][17]=1;matriz[18][17]=1;matriz[19][17]=1;matriz[20][17]=1;matriz[21][17]=1;matriz[22][17]=1;matriz[23][17]=1;matriz[24][17]=1;matriz[19][17]=1;
+        }
+    printf("\n\n");
+    printf("\a\a");
+    system("color 4");
+    printf("Eu te amo minha gatenha me aguarde que trarei mais surpresas a quem eu\ntanto adoro!\n\n\n\n\n");
+
+    system("title Te Amo");
+    system("color 4");
+    system("pause");
+    return 0;
+}
